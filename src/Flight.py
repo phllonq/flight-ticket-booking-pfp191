104faf58af0517c6a1662a530bb61904336628b4
p.loqn.qipq <190559996+phllonq@users.noreply.github.com>
2026-02-05 22:59:06 +0700

    Refactor Flight class for cleaner code structure

--- src/Flight.py
+++ src/Flight.py
@@ -1,14 +1,12 @@
 class Flight:
 
-
     def __init__(self, flight_code, departure, destination, schedule):
-
         self.__flight_code = flight_code
         self.__departure = departure
         self.__destination = destination
         self.__schedule = schedule
         self.__booking_list = []
-        self.__available_seats = 50  # Tổng số ghế 
+        self.__available_seats = 50
 
     def get_flight_code(self):
         return self.__flight_code
@@ -26,22 +24,19 @@ class Flight:
         return self.__available_seats
 
     def add_booking(self, booking):
-
         self.__booking_list.append(booking)
 
     def update_available_seats(self):
-
         self.__available_seats = 50
         for booking in self.__booking_list:
             if booking.get_status() == 'confirmed':
                 self.__available_seats -= booking.get_no_of_seats()
 
     def __str__(self):
-
         return (
             'Flight Code: ' + self.__flight_code + '\n'
-            'Departure: ' + self.__departure + '\n'
-            'Destination: ' + self.__destination + '\n'
+            + 'Departure: ' + self.__departure + '\n'
+            + 'Destination: ' + self.__destination + '\n'
             + str(self.__schedule) + '\n'
-            'Available Seats: ' + str(self.__available_seats)
+            + 'Available Seats: ' + str(self.__available_seats)
         )
