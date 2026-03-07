a39de4cbae4df678b05728371b5de17418128efb
p.loqn.qipq <190559996+phllonq@users.noreply.github.com>
2026-03-07 08:18:19 +0700

    Remove package description from __init__.py
    Removed docstring describing core modules of the system.

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,3 @@
-'''
-This package contains core modules of the system, including:
-- Flight
-- FlightSchedule
-- Booking
-- AirlineSystem
-'''
-
-
 from .Flight import Flight
 from .FlightSchedule import FlightSchedule
 from .Booking import Booking
